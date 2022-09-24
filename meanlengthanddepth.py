0a38b948354b69dd33bb5dd235e0cdfd76f4fec8
Brad <bradhartmail@gmail.com>
2022-09-24 14:02:10 +0930

    Update meanlengthanddepth.py

--- meanlengthanddepth.py
+++ meanlengthanddepth.py
@@ -1,6 +1,6 @@
 #Can be run after running samscript to generate visualisations of contig quality data
 ## needs a separate environment at this stage due to incompatibility with current tormes build.
-# conda create -n datapane -c bioconda datapane
+# conda create -n datapane -c conda-forge datapane=0.14.0 plotly
 # conda activate datapane
 # run this file with an argument of tormes output directory
 # example: meanlengthanddepth.py /home/jblogs/tormesoutputdirectory
