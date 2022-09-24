c18eb64cc6753f40893f76f9a4d18fc65066f6b2
Brad <bradhartmail@gmail.com>
2022-09-24 13:44:37 +0930

    Update meanlengthanddepth.py

--- meanlengthanddepth.py
+++ meanlengthanddepth.py
@@ -1,14 +1,23 @@
+#Can be run after running samscript to generate visualisations of contig quality data
+## needs a separate environment at this stage due to incompatibility with current tormes build.
+# conda create -n datapane -c bioconda datapane
+# conda activate datapane
+# run this file with an argument of tormes output directory
+# example: meanlengthanddepth.py /home/jblogs/tormesoutputdirectory
+
+import sys
 import pandas as pd
 import datapane as dp
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
-meta = pd.read_csv('metadata.txt', sep='\t')
+OUTWD = sys.argv[1]
+meta = pd.read_csv(OUTWD + '/report_files/metadata.txt', sep='\t')
 seqs = meta['Samples'].tolist()
 graphs = []
 
 def qualgraph(seqname):
-    df = pd.read_csv(seqname + 'contlengdepqual.csv')
+    df = pd.read_csv(OUTWD + "/report_files/" + seqname + 'contlengdepqual.csv')
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(go.Bar(x=df['contig'], y=df['meandepth'], name="Mean Depth"), secondary_y=False)
     fig.add_trace(go.Scatter(x=df['contig'], y=df['length'], name="Contig Length"), secondary_y=True)
@@ -27,4 +36,4 @@ report = dp.Report(
         )
     )
 
-report.save(path="Depth coverage report.html")
+report.save(path=OUTWD + "/report_files/Depth_coverage_report.html")
